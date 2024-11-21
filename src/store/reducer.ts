98c02c29b275ed5569b665e7ca98965b4dd7f3ba
vcb88 <129554845+vcb88@users.noreply.github.com>
2024-11-21 02:49:25 -0800

    Create reducer.ts

---
+++ src/store/reducer.ts
@@ -0,0 +1,29 @@
+import { GameState, GameAction, A, P } from '../types';
+import { replace } from '../utils/board';
+
+export const reducer = (s: GameState, a: GameAction): GameState => {
+  switch(a.type) {
+    case A.PL: return {
+      ...s,
+      board: s.board.map(r => [...r])
+        .map((r, x) => x === a.x ? r.map((c, y) => y === a.y ? a.p : c) : r),
+      ops: s.ops - 1
+    };
+    case A.MV: return {
+      ...s,
+      board: s.board.map(r => [...r])
+        .map((r, x) => x === a.fx ? r.map((c, y) => y === a.fy ? P.N : c) :
+                       x === a.tx ? r.map((c, y) => y === a.ty ? a.p : c) : r),
+      ops: s.ops - 1
+    };
+    case A.RP: return { ...s, board: replace(s.board) };
+    case A.ET: return { ...s, p: s.p === P.A ? P.B : P.A, ops: 2 };
+    default: return s;
+  }
+};
+
+export const initialState: GameState = {
+  board: Array(S).fill(null).map(() => Array(S).fill(P.N)),
+  p: P.A,
+  ops: 2
+};
