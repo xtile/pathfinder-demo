4e1bed677dad6690ad05ba50aea1676e2e2ad884
vcb88 <129554845+vcb88@users.noreply.github.com>
2024-11-21 18:32:04 -0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,7 @@
-export const S = 10;
+import { S } from '@/constants';
+
+
+export { S };  
 export const P = { N: 0, A: 1, B: 2 } as const;
 export const O = { PL: 'place', MV: 'move' } as const;
 export const A = { PL: 'PLACE', MV: 'MOVE', RP: 'REPLACE', ET: 'END' } as const;
@@ -6,6 +9,7 @@ export const A = { PL: 'PLACE', MV: 'MOVE', RP: 'REPLACE', ET: 'END' } as const;
 export type Board = number[][];
 export type Position = { x: number; y: number };
 export type Move = Position & { s: number };
+
 export type GameState = {
   board: Board;
   p: number;
