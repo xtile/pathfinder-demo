1b76da94298cd52425cf073b1a693fe4f9c588d5
vcb88 <129554845+vcb88@users.noreply.github.com>
2024-11-21 19:38:03 -0800

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export const Card = React.forwardRef
+export const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
